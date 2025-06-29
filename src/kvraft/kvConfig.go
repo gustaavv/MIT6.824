4cd6528fb6546dd270c13b7614739842bd4efc81
gustav <gustaavv.git@yahoo.com>
2025-06-30 06:48:10 +0800

    update

--- src/kvraft/kvConfig.go
+++ src/kvraft/kvConfig.go
@@ -17,9 +17,17 @@ const ENABLE_TEST_VERBOSE = false
 /////////////////////////// client parameters /////////////////////////////////
 
 const ENABLE_QUERY_SERVER_STATUS = true
+
+// QUERY_SERVER_STATUS_FREQUENCY this value can be set to 200ms (or even lower)
+// on local machine to boost performance.
 const QUERY_SERVER_STATUS_FREQUENCY = 300 * time.Millisecond
 
-const REQUEST_TIMEOUT = 1000 * time.Millisecond
+// REQUEST_TIMEOUT this value can be set to 300ms (or even lower) on local
+// machine to boost performance. But when running on GitHub Action, this error
+// sometimes happen:
+// race: limit on 8128 simultaneously alive goroutines is exceeded, dying
+// So, I set it large enough to safely run GitHub Action.
+const REQUEST_TIMEOUT = 500 * time.Millisecond
 
 // CACHE_SIZE number of last responses cached. set to -1 to disable this feature
 const CACHE_SIZE = 10
